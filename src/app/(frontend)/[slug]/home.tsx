4766c7ed59648fe5acb2c282cb8b86bdabf7812e
Alexey Karasev <enotracoon2020@gmail.com>
2025-11-03 12:36:00 +0700

    Added home page title

--- src/app/(frontend)/[slug]/home.tsx
+++ src/app/(frontend)/[slug]/home.tsx
@@ -1,12 +1,16 @@
 'use server'
 
-import Specialties from '@/components/Specialties'
-import SearchBar from '@/components/SearchBar'
+import getI18n from '@/i18n/getI18n'
 import Hero from '@/components/Hero'
+import SearchBar from '@/components/SearchBar'
+import Specialties from '@/components/Specialties'
+import PageMetaTitle from '@/components/PageMetaTitle'
 
 export default async function Home() {
+  const { t } = await getI18n()
   return (
     <>
+      <PageMetaTitle>{t('general:appName')}</PageMetaTitle>
       <Hero />
       <SearchBar />
       <div className="flex flex-col gap-4 items-center pt-5">
