fb6fda73c43dc3127ffa50486a3db9d37568b0e0
Alexey Karasev <enotracoon2020@gmail.com>
2025-11-04 15:20:57 +0700

    Updated og:description

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -2,11 +2,11 @@ import type { Metadata } from 'next'
 import { getServerSideURL } from './getURL'
 
 const appName = 'Работа и Найм' // todo: extract to i18n
-const appDescription = 'Портал о поиске рабоы и подоаботки' // todo: extract to i18n
+const appDescription = 'Портал о поиске работы и подработки' // todo: extract to i18n
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: appDescription,
   images: [
     {
       url: `${getServerSideURL()}/website-template-OG.webp`,
